b7a4208290c1ff6b1f04ca53928b6f9198dfbf19
saravanakumar505 <saravanakumar@86192@gmail.com>
2026-01-24 17:03:34 +0530

    slider image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   // ✅ Correct slide array (no JSX inside)
  const slides = [
-  "/ecommerce-frontend/public/images/nike.png",
+  "/images/nike.png",
   "/images/skechers_sale.jpg",
   "/images/nike_air_force.jpg"
 ];
